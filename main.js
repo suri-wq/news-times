cc90231a45b4058f20a29f4ff91674543df35687
yiseul <nisuri@knou.ac.kr>
2025-02-21 02:05:41 +0900

    기사 출력, 모바일 nav 추가

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// news 
+// let newsArea = document.getElementById("news-area");
+
+// API
 const API_KEY = `a06fc92583d340f7b2cf962b0eede9ec`
 let news = []
 const getLatestNews =async ()=>{
@@ -6,6 +10,36 @@ const getLatestNews =async ()=>{
     const data = await response.json();
     news = data.articles;
     console.log("rrr", news);
+    render();
 }
 
 getLatestNews();
+
+
+
+function render(){
+    let resultHTML = '';
+
+    for (let i=0;i<news.length;i++){
+        resultHTML += `
+            <div class="row news">
+                <div class="col-lg-4">
+                    <img class="news-img-size" src="${news[i].urlToImage}" alt="">
+                </div>
+                <div class="col-lg-8">
+                    <h2>${news[i].title}</h2>
+                    <p>
+                        ${news[i].description}
+                    </p>
+                    <div>
+                        ${news[i].source.name} ${news[i].publishedAt}
+                    </div>
+                </div>
+            </div>    
+                `
+    }
+    document.getElementById("news-rows").innerHTML=resultHTML;
+    console.log(news.length)
+}
+
+
